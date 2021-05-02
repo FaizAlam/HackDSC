cbde42b30770afe838be28899afa7ddbacd6e262
chinmaychahar <chinmay.cc.06@gmail.com>
2021-05-02 13:53:59 +0530

    commit 1

--- app/src/main/java/com/example/hackdsc/mentorship.java
+++ app/src/main/java/com/example/hackdsc/mentorship.java
@@ -16,15 +16,5 @@ public class mentorship extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_mentorship);
-
-        popularRecycler = findViewById(R.id.most_popular_recycler);
-
-        popularRecycler();
-    }
-
-    private void popularRecycler() {
-
-        popularRecycler.setHasFixedSize(true);
-        popularRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false));
     }
 }
